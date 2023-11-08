3dcad68d1a8d2e4474bce5dc8a43ee4fb01be67c
makiwumi <mfakiwumi@gmail.com>
2023-11-08 00:10:59 -0500

    added logo and mui

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,3 +1,8 @@
-const Discover = () => <div>Discover</div>;
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+    console.log(genres)
+}
 
 export default Discover;
